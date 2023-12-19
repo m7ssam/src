a3517ab317ce15018ae651a86be8589ebef39d8f
m7ossam <mohamed7ossam@outlook.com>
2023-12-19 15:30:04 +0200

    log removed

--- home/views.py
+++ home/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Home Page for the app
-@login_required()
+
 def home(request):
   return render(request, 'home/home.html')
 
 
-@login_required()
+
 def documentation(request):
   return render(request, 'home/documentation.html')
 
 
-@login_required()
+
 def underconstruction(request):
   return render(request, 'underconstruction.html')
--- project/deployment.py
+++ project/deployment.py
@@ -4,7 +4,7 @@ from .settings import BASE_DIR
 
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], "ptj-ops.azurewebsites.net", "20.87.80.68"]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
